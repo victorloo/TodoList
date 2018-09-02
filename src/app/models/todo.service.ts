de98f4caabbcbd9f51970dbc349663fd99213b61
victorloo <ila9970@gmail.com>
2018-09-02 21:45:55 +0800

    Built data in todo.service

--- src/app/models/todo.service.ts
+++ src/app/models/todo.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Todo } from './../prototype/todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+
+  todoes: Todo[] = [
+    {
+      id: 1,
+      content: 'Hit the gym',
+      checked: false
+    },
+    {
+      id: 2,
+      content: "Pay bills",
+      checked: true
+    },
+    {
+      id: 3,
+      content: "Meet George",
+      checked: false
+    },
+    {
+      id: 4,
+      content: "Buy eggs",
+      checked: false
+    },
+    {
+      id: 5,
+      content: "Read a book",
+      checked: false
+    },
+    {
+      id: 6,
+      content: "Organize office",
+      checked: false
+    }
+  ];
 
   constructor() { }
 }
