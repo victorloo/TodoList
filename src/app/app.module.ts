f79e560a24f23f98b6f57f2103c773fcab86aef4
victorloo <ila9970@gmail.com>
2018-09-02 21:43:25 +0800

    Import TodoService to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { AddFormComponent } from './add-form/add-form.component';
+import { TodoService } from './models/todo.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { AddFormComponent } from './add-form/add-form.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
