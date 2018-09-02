8c84ccc6744618d20e77dad0f4453893e18f945f
victorloo <ila9970@gmail.com>
2018-09-02 22:02:14 +0800

    Removing unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Todo } from './prototype/todo';
 
 @Component({
   selector: 'app-root',
@@ -8,49 +7,4 @@ import { Todo } from './prototype/todo';
 })
 export class AppComponent {
   title: string = 'My To Do List';
-
-  todoes: Todo[] = [
-    {
-      id: 1,
-      content: 'Hit the gym',
-      checked: false
-    },
-    {
-      id: 2,
-      content: "Pay bills",
-      checked: true
-    },
-    {
-      id: 3,
-      content: "Meet George",
-      checked: false
-    },
-    {
-      id: 4,
-      content: "Buy eggs",
-      checked: false
-    },
-    {
-      id: 5,
-      content: "Read a book",
-      checked: false
-    },
-    {
-      id: 6,
-      content: "Organize office",
-      checked: false
-    }
-  ];
-
-  addTodo(content: string) {
-    this.todoes.push({
-      id: this.todoes.length +1,
-      content: content,
-      checked: false
-    })
-  }
-
-  deleteTodo(todo: Todo) {
-    this.todoes = this.todoes.filter(thatTodo => thatTodo.id !== todo.id);
-  }
 }
